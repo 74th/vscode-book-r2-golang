cd9a1d9e6e4e7c1f2a4af31b5674e322d93c1ad8
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2023-09-15 10:55:59 +0900

    コメント追加

--- domain/usecase/repository.go
+++ domain/usecase/repository.go
@@ -6,8 +6,12 @@ import (
 
 // タスクデータベース
 type TaskDatabase interface {
+	// タスクの追加
 	Add(*entity.Task) (int, error)
+	// 未完了のタスク一覧
 	SearchUnfinished() ([]*entity.Task, error)
+	// タスクの更新
 	Update(*entity.Task) error
+	// タスクの取得
 	Get(id int) (*entity.Task, error)
 }
